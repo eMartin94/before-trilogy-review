3f64940ee95d645033c99904f3025514561cba58
Martin Pizango <eduardo.martin688@gmail.com>
2023-10-25 14:33:04 -0500

    env next-config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  env: {
+    EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    EMAILJS_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+    EMAILJS_PUBLIC_KEY: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY,
+  },
 };
 
 const withNextIntl = require('next-intl/plugin')('./src/i18n.js');
