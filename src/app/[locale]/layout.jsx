4339e8cd28320821534cda5b2ac6a5a49d09ceea
Martin Pizango <eduardo.martin688@gmail.com>
2023-10-25 18:13:41 -0500

    updating metadata

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -7,9 +7,32 @@ import { Urbanist } from 'next/font/google';
 const urbanist = Urbanist({ subsets: ['latin'], weights: [400, 600, 800] });
 
 export const metadata = {
+  // metadataBase: 'http://localhost:3000',
+  metadataBase: new URL('https://before-trilogy-review.vercel.app'),
   title: 'Trilogy Before',
   description:
     'La trilogía "Before" es un logro cinematográfico que nos invita a reflexionar sobre el amor, la conexión humana y el paso del tiempo de una manera que pocas otras películas logran',
+  keywords:
+    'trilogía, before, película, cinematográfica, logro, reflexión, amor, conexión, humana, paso, tiempo',
+  openGraph: {
+    type: 'website',
+    url: 'https://yourwebsite.com',
+    title: 'Trilogy Before',
+    description:
+      'The "Before" trilogy is a cinematic achievement that invites us to reflect on love, human connection, and the passage of time in a way that few other films achieve.',
+    image:
+      'https://res.cloudinary.com/dxbbbewxt/image/upload/v1698267740/before-trilogy/poster_un9yfu.webp',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@eMartiiin94',
+    creator: '@eMartiiin94',
+    title: 'Trilogy Before',
+    description:
+      'The "Before" trilogy is a cinematic achievement that invites us to reflect on love, human connection, and the passage of time in a way that few other films achieve.',
+    image:
+      'https://res.cloudinary.com/dxbbbewxt/image/upload/v1698267740/before-trilogy/poster_un9yfu.webp',
+  },
 };
 
 export default async function RootLayout({ children, params: { locale } }) {
